b25dffb5f9c0d84298a84db11c9185dad8a4c76e
JonaSaija <128484354+JonaSaija@users.noreply.github.com>
2024-09-05 17:01:09 +0200

    Better UI

---
+++ closeAction.js
@@ -0,0 +1,25 @@
+// class Test{
+
+//     let menuClose = false;
+
+//     const button = document.getElementById(menuButton)
+//     const div = document.getElementById(navMenuParent)
+
+//     button.onclick = function()
+//     {
+//         menuClose = !menuClose;
+//     }
+
+//     function buttonClick(pageName)
+//     {
+//         console.log("Button is clicked for page: " + pageName)
+//     }
+
+//     if(menuClose){
+//         div.className = "closeNav"
+//     }else{
+//         div.className = "openNav"
+//     }
+
+//     console.log(menuClose);
+// }
