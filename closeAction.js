ef0f4c9ffc35d290c1ab687452b9a691ac239f61
JonaSaija <128484354+JonaSaija@users.noreply.github.com>
2024-09-09 13:07:02 +0200

    Fix most import into html files

--- closeAction.js
+++ closeAction.js
@@ -1,22 +1,22 @@
     let menuClose = false;
-
-    const button = document.getElementById(menuButton)
-    const div = document.getElementById(navMenuParent)
-
-    button.onclick = function()
-    {
-        menuClose = !menuClose;
-    }
-
-    function buttonClick(pageName)
-    {
-        console.log("Button is clicked for page: " + pageName)
-    }
-
-    if(menuClose){
-        div.className = "closeNav"
-    }else{
-        div.className = "openNav"
-    }
-
-    console.log(menuClose);
+  
+      const button = document.getElementById(menuButton)
+      const div = document.getElementById(navMenuParent)
+  
+      button.onclick = function()
+      {
+          menuClose = !menuClose;
+      }
+  
+      function buttonClick(pageName)
+      {
+          console.log("Button is clicked for page: " + pageName)
+      }
+  
+      if(menuClose){
+          div.className = "closeNav"
+      }else{
+          div.className = "openNav"
+      }
+  
+      console.log(menuClose);
