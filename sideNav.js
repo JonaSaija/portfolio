6fdbef362f8b38eaa934fca53f787687c4c3af90
JonaSaija <128484354+JonaSaija@users.noreply.github.com>
2024-09-11 10:35:28 +0200

    menu finally works dynamically

--- sideNav.js
+++ sideNav.js
@@ -2,12 +2,10 @@
 class SideNav extends HTMLElement {
     constructor() {
       super();
-  
-      // Create a container element for the card
+
       const container = document.createElement('div');
       container.classList.add('side-nav');
-  
-      // Define the HTML content for the card
+
       container.innerHTML = `
        <nav class="navBar">
       <div class="closeNavButtonDiv">
@@ -20,7 +18,7 @@ class SideNav extends HTMLElement {
           <button class="cybr-btn">
             <span class="changeBackstoryTextSmallerScreen"></span>             
             <span aria-hidden="" class="cybr-btn_glitch">
-            <span class="changeBackstoryTextSmallerScreen"></span>             
+            <span class="changeBackstoryTextSmallerScreen id="dynamicTextButton""></span>             
             </span>
             <span aria-hidden="" class="cybr-btn_tag">2005</span>
           </button>
@@ -29,7 +27,7 @@ class SideNav extends HTMLElement {
           <button class="cybr-btn">
             <span class="changeLevelTextSmallerScreen"></span>
             <span aria-hidden="" class="cybr-btn_glitch">
-            <span class="changeLevelTextSmallerScreen"></span>
+            <span class="changeLevelTextSmallerScreen" id="dynamicTextButton"></span>
             </span>
             <span aria-hidden="" class="cybr-btn_tag">2005</span>
           </button>
@@ -38,7 +36,9 @@ class SideNav extends HTMLElement {
         <form action="./blogPage.html"class="navButton">
           <button class="cybr-btn">
             <span class="BlogButtonText"></span>
-            <span aria-hidden="" class="cybr-btn_glitch">Blog_</span>
+            <span aria-hidden="" class="cybr-btn_glitch">
+            <span id="dynamicTextButton" class=""></span>
+            </span>
             <span aria-hidden="" class="cybr-btn_tag">2005</span>
           </button>
         </form>
@@ -46,32 +46,36 @@ class SideNav extends HTMLElement {
         <form action="./ncpdDataPage.html"class="navButton">
           <button class="cybr-btn">
             <span class="NcpdDataButtonText"></span>
-            <span aria-hidden="" class="cybr-btn_glitch">Ncpd data_</span>
+               <span aria-hidden="" class="cybr-btn_glitch">
+            <span id="dynamicTextButton" class=""></span>
+            </span>
             <span aria-hidden="" class="cybr-btn_tag">2005</span>
           </button>
         </form>
         <form action="./gigsPage.html"class="navButton">
           <button class="cybr-btn">
             <span class="GigsButtonText"></span>
-            <span aria-hidden="" class="cybr-btn_glitch">Gig's_</span>
+               <span aria-hidden="" class="cybr-btn_glitch">
+            <span id="dynamicTextButton" class=""></span>
+            </span>
             <span aria-hidden="" class="cybr-btn_tag">2005</span>
           </button>
         </form>
         <form action="./contactInfo.html"class="navButton">
           <button class="cybr-btn">
             <span class="CyberContactButtonText"></span>
-            <span aria-hidden="" class="cybr-btn_glitch"
-              >Cyber contact_</span
-            >
+               <span aria-hidden="" class="cybr-btn_glitch">
+            <span id="dynamicTextButton" class=""></span>
+            </span>
             <span aria-hidden="" class="cybr-btn_tag">2005</span>
           </button>
         </form>
         <form action="./infoPage.html"class="navButton">
           <button class="cybr-btn">
             <span class="CharacterInfoButtonText"></span>
-            <span aria-hidden="" class="cybr-btn_glitch"
-              >Character info_</span
-            >
+               <span aria-hidden="" class="cybr-btn_glitch">
+            <span id="dynamicTextButton" class=""></span>
+            </span>
             <span aria-hidden="" class="cybr-btn_tag">2005</span>
           </button>
         </form>
@@ -92,17 +96,14 @@ class SideNav extends HTMLElement {
   </nav>
       `;      
       
-      // Append the container directly to the custom element without Shadow DOM
       this.appendChild(container);
     }
 
     getCurrentPage() {
-      // Get the current page's filename (e.g., 'index.html' from '/index.html' or './index.html')
       return window.location.pathname.split('/Pages/').pop();
     }
   
     getFormActionPage(actionUrl) {
-      // Get the filename from the form action attribute (e.g., 'index.html' from './index.html')
       return actionUrl.split('./').pop();
     }
     
@@ -111,26 +112,24 @@ class SideNav extends HTMLElement {
       }
       
       changeCurrentPageButton() {
-        const currentPage = this.getCurrentPage(); // Get the current page path
+        const currentPage = this.getCurrentPage();
         const forms = this.querySelectorAll('form');
         
         forms.forEach(form => {
-          let formAction = this.getFormActionPage(form.getAttribute('action')); // Get form action attribute
+          let formAction = this.getFormActionPage(form.getAttribute('action')); 
           
-          console.log("Value of current page:" + currentPage)
-          console.log("Value of button action:" + formAction)
-          if (currentPage === formAction) { // Check if the current page matches the form action
+          if (currentPage === formAction) {
             const buttonText = form.querySelector('span');
-            console.log("Value of button/buttontext:" + buttonText.className)
-            formAction = "/index.html"
-            console.log("Value of button action must have changed:" + formAction)
-            buttonText.className = "changeToHomepage"; // Changes the button of that page to homepage button.
-            console.log("Value of button/buttontext must have changed:" + buttonText.className)
+            const buttonDeepText = form.getElementsByTagName('dynamicTextButton');
+            form.setAttribute('action','/portfolio')
+            // TODO: fix this f*cking class changing!!!!
+            buttonText.className = "changeToHomepage";
+            buttonDeepText.className = "changeToHomepage";
+            console.log(buttonDeepText.className)
           }
         });
       }
 }
   
-  // Register the custom element
   customElements.define('side-nav', SideNav);
   
